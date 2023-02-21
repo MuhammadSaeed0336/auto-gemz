8e1233847330c307ed3a9e3d84d291a70e93ab19
Qasim Malik <iamqasimk@gmail.com>
2023-02-21 12:09:02 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Dashboard/DashComponents/SingleCArd.jsx

--- src/components/Dashboard/DashComponents/SingleCArd.jsx
+++ src/components/Dashboard/DashComponents/SingleCArd.jsx
@@ -1,39 +1,56 @@
-import React from "react";
-// import "../styles/dashboard.css";
+import React, { useState } from "react";
+import { useGetAllCarsQuery } from "../../../features/cars/carSlice";
+import { useGetAllFeedsQuery } from "../../../features/feedback/feedbackSlice";
+import { useGetAllNewCarsQuery } from "../../../features/newCars/newCarSlice";
+import { useGetAllUsersQuery } from "../../../features/users/userSlice";
+import DatePicker from "react-date-picker";
 
-const data = [
-  {
-    title: "New Cars",
-    totalNumber: 50,
-    icon: "ri-car-fill",
-  },
-  {
-    title: "Used Cars",
-    totalNumber: 100,
-    icon: "ri-steering-2-line",
-  },
-  {
-    title: "Users",
-    totalNumber: "50k",
-    icon: "ri-user-line",
-  },
-  {
-    title: "Feedbacks",
-    totalNumber: 1000,
-    icon: "ri-feedback-line",
-  },
-  {
-    title: "Rented Out Cars",
-    totalNumber: 1000,
-    icon: "ri-money-dollar-box-line",
-  },
-  {
-    title: "Cars For Rent",
-    totalNumber: 1000,
-    icon: "ri-shopping-cart-fill",
-  },
-];
+
 const SingleCArd = () => {
+  let time = new Date().toLocaleTimeString();
+  const [value, onChange] = useState(new Date());
+  const [ctime, setTime] = useState(time);
+  const newCars = useGetAllNewCarsQuery()
+  const usedCars = useGetAllCarsQuery()
+  const allUsers = useGetAllUsersQuery()
+  const allFeeds = useGetAllFeedsQuery()
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  };
+  setInterval(UpdateTime);
+  const data = [
+    {
+      title: "New Cars",
+      totalNumber: newCars.data?.cars.length,
+      icon: "ri-car-fill",
+    },
+    {
+      title: "Used Cars",
+      totalNumber: usedCars.data?.cars.length,
+      icon: "ri-steering-2-line",
+    },
+    {
+      title: "Users",
+      totalNumber: allUsers.data?.users.length,
+      icon: "ri-user-line",
+    },
+    {
+      title: "Feedbacks",
+      totalNumber: allFeeds.data?.data.length,
+      icon: "ri-feedback-line",
+    },
+    {
+      title: "Rented Out Cars",
+      totalNumber: 1000,
+      icon: "ri-money-dollar-box-line",
+    },
+    {
+      title: <DatePicker className="bg-secondary" onChange={onChange} value={value}/>,
+      totalNumber: ctime,
+      icon: "ri-time-line",
+    },
+  ];
   return (
     <>
       <div
@@ -72,7 +89,7 @@ const SingleCArd = () => {
                   </span>
                   <div className="card_content">
                     <h4>{dat.title}</h4>
-                    <span>{dat.totalNumber}+</span>
+                    <span style={{fontSize:"1.5rem", fontWeight:"bold", color:"red"}}>{dat.totalNumber}</span>
                   </div>
                 </div>
               </div>
